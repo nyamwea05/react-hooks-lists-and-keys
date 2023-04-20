e9deadce5eca34bae1321434f999d4ce3fc223e4
sandra <nyamweasandra@gmail.com>
2023-04-20 13:08:19 +0300

    Completed assingment

---
+++ src/components/.NavBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+function NavBar() {
+  const links = ["home", "about", "projects"];
+  return <nav className="nav"> {<a href="#home">home</a>}
+  {<a href="#about">about</a>}
+  {<a href="#projects">projects</a>}
+  </nav>;
+}
+export default NavBar;
